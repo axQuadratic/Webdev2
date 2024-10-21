1149183b3b44663c8a65fd5106538bfc09d5cf45
Nils Konradsson <niko0416@storsjogymnasiet.se>
2024-10-21 09:56:28 +0200

    Harmony

--- Harmony Chat/app.js
+++ Harmony Chat/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const cors = require('cors');
 const sql = require('mysql2')
-const bodyParser = require('body-parser');
 const PORT = 8181;
 
 const connection = sql.createConnection({
@@ -15,20 +15,40 @@ const connection = sql.createConnection({
 connection.connect();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
     res.sendFile("public/index.html", { root: __dirname });
 });
 
-app.get("/messages/", function(req, res) {
+app.get("/getmessage/", function(req, res) {
     connection.promise().query('SELECT * FROM messages') 
     .then(function([rows, fields]) {
         res.send(rows)
     })
     .catch(function() { res.sendStatus(500) });
+});
+
+app.post("/sendmessage/", function(req, res) {
+    let timestamp = Math.floor(Date.now() / 1000)
+    connection.promise().query(`INSERT INTO messages (message, username, time) VALUES ('${req.body.message}', '${req.body.username}', ${timestamp})`)
+    .then(function([rows, fields]) {
+        res.sendStatus(200);
+    })
+    .catch(function(e) { console.log(e); res.sendStatus(500); });
+});
+
+app.post("/reset/", function(req, res){
+    if (req.body.password != "qwe123!!"){
+        res.sendStatus(451);  
+        return;
+    } 
+    connection.promise().query("TRUNCATE TABLE messages")
+    .then(function([rows, fields]){
+        res.sendStatus(200);
+    });
 });
 
 app.listen(PORT, function() {
--- Harmony Chat/public/script.js
+++ Harmony Chat/public/script.js
@@ -1,31 +1,112 @@
-let messageBox = '<div class="box"><h3 class="messageuser">ERROR</h3><p class="messagebody">Failed to load message...</p><b class="messagetime">00:00, Jan 1, 1970</b></div>';
-
 let ids = [];
+let server_ip = "localhost:8181"
 
 async function getMessage() {
+    try {
+        let req = new XMLHttpRequest();
+
+        req.onreadystatechange = function() {
+            if (req.readyState === 4) {
+                parseMessages(req.responseText);
+            }
+        }
+
+        req.open("GET", `http://${server_ip}/getmessage`, true);
+        req.send();
+    }
+    catch { return; }
+}
+
+function parseMessages(text) {
+    messageContainer.innerHTML = "<p class='ubuntu-medium'>No messages found. There may not be a server at this address.</p>"
+    document.getElementById("messages").style.borderStyle = "solid";
+
+    messageText = ""
+    JSON.parse(text).reverse().forEach(function(message) {
+
+        ids.push(message.id);
+
+        let timestamp = new Date(message.time * 1000);
+        let year = timestamp.getFullYear();
+        let month = timestamp.getMonth();
+        let day = timestamp.getDate();
+        let hour = timestamp.getHours();
+        let minute = timestamp.getMinutes();
+        
+        let time = `${(hour).toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}, ${year}/${(month + 1).toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}`;
+        
+        let messageBox = `<div class="box"><h3 class="messageuser">${message.username}</h3><p class="messagebody">${message.message}</p><b class="messagetime">${time}</b></div>`;
+        messageText += messageBox;
+    });
+
+    if (messageText != "") messageContainer.innerHTML = messageText;
+}
+
+function sendMessage() {
+    let username = document.getElementById("user_input").value;
+    let message = document.getElementById("messageadd").value;
+    if (username === "" || message === "") return;
+
+    document.getElementById("messageadd").value = "";
+
     let req = new XMLHttpRequest();
 
     req.onreadystatechange = function() {
         if (req.readyState === 4) {
-            parseMessages(req.responseText)
-        };
-    };
-
-    req.open("GET", "http://localhost:8181/messages", true)
-    req.send();
-};
-
-function parseMessages(text) {
-    JSON.parse(text).reverse().forEach(message => {
-        console.log(message.id)
-        if (message.id in ids) {
-            return;
+            getMessage();
         }
-        
-        messageContainer.innerHTML += messageBox;
-    });
+    }
+    
+    req.open("POST", `http://${server_ip}/sendmessage`, true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.send(JSON.stringify({ "message": message, "username": username }));
 }
 
-setInterval(getMessage, 500);
+function connectToServer() {
+    server_ip = document.getElementById("server_input").value
+    messageContainer.innerHTML = "<p class='ubuntu-medium'>No messages found. There may not be a server at this address.</p>"
+    document.getElementById("messages").style.borderStyle = "none";
+    ids = [];
+    getMessage();
+}
 
 let messageContainer = document.getElementById("messages");
+document.getElementById("server_input").addEventListener("input", connectToServer);
+getMessage();
+setInterval(getMessage, 500);
+
+let image = Math.floor(Math.random() * 8) + 1;
+let poweredByText = "powered by "
+switch (image) {
+    case 1:
+        poweredByText += "BARACK OBAMA";
+        break;
+    case 2:
+        poweredByText += "OBAMIUM";
+        break;
+    case 3:
+        poweredByText += "MONKÉ";
+        break;
+    case 4:
+        poweredByText += "HENTAI";
+        break;
+    case 5:
+        poweredByText += "SHREK";
+        break;
+    case 6:
+        poweredByText += "BEN SHAPIRO";
+        break;
+    case 7:
+        poweredByText += "HOT DOG";
+        break;
+    case 8:
+        poweredByText += "THE POWER TRIO";
+        break;
+}
+
+document.getElementById("powered-by").innerHTML = poweredByText;
+document.body.style.backgroundImage = `url(images/${image}.png)`;
+document.getElementById("messageadd").addEventListener("keypress", function(e) {
+    if (e.key != "Enter") return;
+    sendMessage();
+});
